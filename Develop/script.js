82385f080d8a3a012470834ea7ee95707e7b50eb
Phenol Lully <lullyp95@gmail.com>
2023-10-23 23:15:06 -0400

    Added code to Script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function getRandomNumber(max) {
+  var randomNumber=Math.random() * (max+1);
+  return Math.floor(randomNumber);
+}
+
+console.log(getRandomNumber(10));
+console.log(getRandomNumber(5));
+console.log(getRandomNumber(3));
+
+
+function getRandomElementFromArray(array) {
+  var randomPosition=getRandomNumber(array.length-1);
+  return array[randomPosition];
+}
+var letters=['a', 'b', 'c','d'];
+console.log(getRandomElementFromArray(letters));
+console.log(getRandomElementFromArray([1,2,7,9,10]));
+console.log(getRandomElementFromArray(['?,', '/', '$']));
+
+function generatePassword() {
+  var char= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'+
+               'abcdefghijklmnopqrstuvwxyz0123456789#$';
+  var password=""; 
+  for (var i=0; i<=8; i++) {
+    
+    
+
+
+    return password;
+  }
+
+    
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
